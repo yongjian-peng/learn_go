c179a03b2862aec58acbf59885c66dfa4f04787c
yong <yong@blueoceanpay.com>
2023-12-13 07:55:12 +0800

    20231213

---
+++ golibmain/concurrency/class4/queue/queue.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SliceQueue struct {
+	data []interface{}
+	mu   sync.Mutex
+}
+
+func main() {
+	var wg sync.WaitGroup
+	queue := NewQueue(100)
+	wg.Add(2)
+	go func() {
+		wg.Done()
+		for i := 0; i < 100; i++ {
+			queue.Enqueue(i)
+		}
+	}()
+	go func() {
+		for {
+			v := queue.Dequeue()
+			fmt.Println("出队的是：", v)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	wg.Done()
+	wg.Wait()
+}
+
+func NewQueue(n int) *SliceQueue {
+	return &SliceQueue{
+		data: make([]interface{}, 0, n),
+	}
+}
+
+// 入队，把值放在队尾
+func (q *SliceQueue) Enqueue(v interface{}) {
+	q.mu.Lock()
+	q.data = append(q.data, v)
+	q.mu.Unlock()
+}
+
+// 出队，移出对头并返回
+func (q *SliceQueue) Dequeue() interface{} {
+	q.mu.Lock()
+
+	// 如果队列为空，直接返回
+	if len(q.data) == 0 {
+		q.mu.Unlock()
+		return "队列没有数据了"
+	}
+	v := q.data[0]
+	// 移除对头
+	q.data = q.data[1:]
+	q.mu.Unlock()
+	return v
+}
